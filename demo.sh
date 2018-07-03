814460cde4e40758aeab73cb35200e881ce32d65
jackrain <jackrsky@gmail.com>
2018-07-03 23:55:20 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -30,7 +30,7 @@ eureka_service_url=$config_server_url/eureka/
 portal_url=http://localhost:8070
 
 # JAVA OPTS
-BASE_JAVA_OPTS="-Denv=dev -Ddev_meta=$config_server_url -Deureka.instance.homePageUrl=http://172.16.8.32:9998" 
+BASE_JAVA_OPTS="-Denv=dev -Ddev_meta=$config_server_url" 
 SERVER_JAVA_OPTS="$BASE_JAVA_OPTS -Dspring.profiles.active=github -Deureka.service.url=$eureka_service_url"
 PORTAL_JAVA_OPTS="$BASE_JAVA_OPTS -Dspring.profiles.active=github,auth"
 
