3e8757cd923a6f7abe13f3e2949d9ae3afb7fd6a
jackrain <jackrsky@gmail.com>
2018-07-03 19:52:59 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://mdmdbtest.fastfish.com:3306/ApolloConfigDB?characterEncoding=utf8
-apollo_config_db_username=apollo
-apollo_config_db_password=apollo
+apollo_config_db_url=jdbc:mysql://172.16.9.107:3306/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_username=root
+apollo_config_db_password=root
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://mdmdbtest.fastfish.com:3306/ApolloPortalDB?characterEncoding=utf8
-apollo_portal_db_username=apollo
-apollo_portal_db_password=apollo
+apollo_portal_db_url=jdbc:mysql://172.16.9.107:3306/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_username=root
+apollo_portal_db_password=root
 
 # =============== Please do not modify the following content =============== #
 
